70616f94786961117926634d686df1100e865bd1
Mirai SHINJO <oss@mshinjo.com>
2025-06-14 12:52:14 -0400

    scripts: run_cppcheck: suppress 'missingInclude' in cppcheck
    The 'missingInclude' check is unnecessary and problematic
    for this Git repository.
    
    See below for an example output from the 'missingInclude' check:
    
    /home/runner/work/finch-flight-software/finch-flight-software/lib/
    ccsds123b/src/image.c:1:0: information:
    Include file: "finch/ccsds123b/image.h" not found. [missingInclude]
    ^
    6/10 files checked, 18% done
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -8,6 +8,7 @@ set -e
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../finch-flight-software-env.sh"
 
 cppcheck --enable=all \
+    --suppress=missingInclude \
     --suppress=missingIncludeSystem \
     --error-exitcode=1 \
     "${FINCH_FLIGHT_SOFTWARE_ROOT}"
